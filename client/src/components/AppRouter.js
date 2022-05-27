082daf63530ee0b1298039dbd57a7f674a85bcca
Denis Alekhin <dennyaa2003@gmail.com>
2022-05-27 13:38:18 +0500

    added routes

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react';
+import {Routes, Route, Navigate } from 'react-router-dom';
+import {authRoutes, publicRoutes} from "../routes";
+import Context from "../index";
+
+const AppRouter = () => {
+    const {user} = useContext(Context);
+    console.log(user);
+    return (
+        <Routes>
+            {/* Маршруты доступные авторизированным пользователям */}
+            {user.isAuth && authRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component />}/>
+            )}
+             {publicRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component />}/>
+            )}
+            <Route path="*" element={<Navigate to="/"/>}/>
+        </Routes>
+    );
+}
+
+export default AppRouter;
