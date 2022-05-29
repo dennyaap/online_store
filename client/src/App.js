ff9d110f632014e1efb115fe0012d3bc00b5b9a8
Denis Alekhin <dennyaa2003@gmail.com>
2022-05-29 20:22:34 +0500

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Route, Navigator} from 'react-router-dom';
 import AppRouter from './components/AppRouter';
+import NavBar from './components/NavBar';
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <AppRouter />
       </BrowserRouter>
     </div>
