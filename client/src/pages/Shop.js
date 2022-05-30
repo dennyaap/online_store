1750928ef7e96ebcca7fe59e1f2f836cc6a06cd7
Denis Alekhin <dennyaa2003@gmail.com>
2022-05-30 15:21:01 +0500

    added typebar

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import TypeBar from '../components/TypeBar';
+
 
 const Shop = () => {
     return (
-        <div>
-            SHOP
-        </div>
+        <Container>
+            <Row className="mt-4">
+                <Col md={3}>
+                    <TypeBar/>
+                </Col>
+                <Col md={9}>
+
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -4,19 +4,22 @@ export default class DeviceStore {
     constructor() {
         this._types = [
             {id: 1, name: 'Холодильники'},
-            {id: 2, name: 'Смартфоны'}
-        ]
+            {id: 2, name: 'Смартфоны'},
+            {id: 3, name: 'Ноутбуки'},
+            {id: 4, name: 'Телевизоры'},
+        ];
         this._brands = [
             {id: 1, name: 'Samsung'},
             {id: 2, name: 'Apple'}
-        ]
+        ];
         this._devices = [
             {id: 1, name: "Iphone 12 pro", price: 96000, rating: 5, img: 'https://app-room76.ru/wp-content/uploads/2020/10/iphone-12-pro-silver-hero.png'},
             {id: 2, name: "Iphone 12 pro", price: 96000, rating: 5, img: 'https://app-room76.ru/wp-content/uploads/2020/10/iphone-12-pro-silver-hero.png'},
             {id: 3, name: "Iphone 12 pro", price: 96000, rating: 5, img: 'https://app-room76.ru/wp-content/uploads/2020/10/iphone-12-pro-silver-hero.png'},
             {id: 4, name: "Iphone 12 pro", price: 96000, rating: 5, img: 'https://app-room76.ru/wp-content/uploads/2020/10/iphone-12-pro-silver-hero.png'},
 
-        ]
+        ];
+        this._selectedType = {};
         makeAutoObservable(this); //теперь mobx будет следить за изменениями этих переменных и при их изменении компоненты будут перерендедориваться
     }
     //создаем экшены
@@ -29,6 +32,10 @@ export default class DeviceStore {
     setDevices(devices) {
         this._devices = devices;
     }
+    setSelectedType(type) {
+        this._selectedType = type;
+    }
+
 
     //computed функции, они вызываются в том случае, если переменная внутри была изменена
     get types() {
@@ -39,5 +46,8 @@ export default class DeviceStore {
     }
     get devices() {
         return this._devices;
+    }
+    get selectedType() {
+        return this._selectedType;
     }
 }
