44412746325384cf15155f321768df8a432266ea
Denis Alekhin <alekhin.den69@gmail.com>
2022-05-31 09:21:13 +0500

    added spiner

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
-import {BrowserRouter, Route, Navigator} from 'react-router-dom';
+import { useContext, useState, useEffect } from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import AppRouter from './components/AppRouter';
 import NavBar from './components/NavBar';
+import { observer } from 'mobx-react-lite';
+import Context from './index';
+import { check } from './http/useAPI';
+import { Spinner } from 'react-bootstrap';
 
+const App = observer(() => {
+  const { user } = useContext(Context);
+  const [loading, setLoading] = useState(true);
 
+  useEffect( () => {
+    check().then(data => {
+      user.setUser(true);
+      user.setIsAuth(true);
+    }).finally( () => {
+      setLoading(false);
+    });
+  }, []);
 
-function App() {
+  if (loading) {
+    return <Spinner animation={'grow'} style= {{position: "fixed", top: "50%", left: "50%", width: 50, height: 50}}/>
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,6 +32,6 @@ function App() {
       </BrowserRouter>
     </div>
   );
-}
+});
 
 export default App;
