23bd84ac27e1da1c786692845fd5a0a6ce2ab1b0
Denis Alekhin <alekhin.den69@gmail.com>
2022-05-31 19:56:42 +0500

    the ability to add a brand

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, Button, Form} from 'react-bootstrap';
+import { createBrand } from '../../http/deviceAPI';
+
 
 const CreateBrand = ({ show, onHide }) => {
+    const [ value, setValue ] = useState(''); 
+    const addBrand = () => {
+        createBrand({ name: value }).then(data => setValue(''));
+        onHide();
+    }
+
     return (
         <div>
             <Modal
@@ -12,19 +20,21 @@ const CreateBrand = ({ show, onHide }) => {
                 >
                 <Modal.Header closeButton>
                     <Modal.Title id="contained-modal-title-vcenter">
-                        Добавить тип
+                        Добавить бренд
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
                         <Form.Control
-                            placeholder='Введите название типа..'
+                            value={value}
+                            onChange={e => setValue(e.target.value)}
+                            placeholder='Введите название бренда..'
                         />
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant='outline-danger' onClick={onHide}>Закрыть</Button>
-                    <Button variant='outline-success' onClick={onHide}>Добавить</Button>
+                    <Button variant='outline-success' onClick={addBrand}>Добавить</Button>
                 </Modal.Footer>
             </Modal>
         </div>
