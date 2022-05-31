38c3cbc6514093cb126bc0bc3f561262f34c309a
Denis Alekhin <alekhin.den69@gmail.com>
2022-05-31 19:00:20 +0500

    added img src

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -13,7 +13,7 @@ const DeviceItem = ({ device }) => {
                 border={'light'}
                 onClick={ () => navigate(DEVICE_ROUTE + '/' + device.id)}
             >
-                <Image width={150} height={150} src={device.img} />
+                <Image width={150} height={150} src={process.env.REACT_APP_API_URL + device.img} />
                 <div className='d-flex justify-content-between align-items-center mt-1 text-black-50'>
                     <div>Apple...</div>
                     <div className='d-flex align-items-center'>
